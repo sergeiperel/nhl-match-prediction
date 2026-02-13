e83cfe45b31eeaf0ffc537bbb13b2343ea0b7407
sergeiperel <sergeip.2002@mail.ru>
2026-02-13 22:16:38 +0300

    compose&html content added

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from redis import Redis
 
 app = FastAPI()
+client = Redis(host="db")
+
+
+@app.get("/health")
+def health():
+    return {"message": "Healthy!"}
 
 
 @app.get("/")
 def test():
     return {"message": "Hello, World!"}
+
+
+@app.get("/api/ok")
+def get_api():
+    return {"API": "OK!"}
+
+
+@app.get("/api/redis/put{x}")
+def put_redis(x: str):
+    client.set(x, "1")
+    return {"message": "OK!"}
